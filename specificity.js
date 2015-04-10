91dabb9b90e9f70124620ae8a179d6a7dbeef962
Kevin Sawicki <kevinsawicki@gmail.com>
2015-04-10 10:03:54 -0700

    Reuse regexes

--- specificity.js
+++ specificity.js
@@ -5,20 +5,62 @@
  * Returns a selector integer value
  */
 
+// The following regular expressions assume that selectors matching the preceding regular expressions have been removed
+var attributeRegex = /(\[[^\]]+\])/g;
+var idRegex = /(#[^\s\+>~\.\[:]+)/g;
+var classRegex = /(\.[^\s\+>~\.\[:]+)/g;
+var pseudoElementRegex = /(::[^\s\+>~\.\[:]+|:first-line|:first-letter|:before|:after)/g;
+var pseudoClassRegex = /(:[^\s\+>~\.\[:]+)/g;
+var elementRegex = /([^\s\+>~\.\[:]+)/g;
+var notRegex = /:not\(([^\)]*)\)/g;
+var ruleRegex = /{[^]*/gm;
+var separatorRegex = /[\*\s\+>~]/g;
+var straysRegex = /[#\.]/g;
+
+// Remove the negation psuedo-class (:not) but leave its argument because specificity is calculated on its argument
+var removeNot = function(selector) {
+	if (notRegex.test(selector)) {
+		selector = selector.replace(notRegex, '     $1 ');
+	}
+
+	return selector;
+}
+
+// Remove anything after a left brace in case a user has pasted in a rule, not just a selector
+var removeRules = function(selector) {
+	if (ruleRegex.test(selector)) {
+		var matches = selector.match(ruleRegex);
+		for (var i = 0; i < matches.length; i++) {
+			selector = selector.replace(matches[i], '');
+		}
+	}
+
+	return selector;
+}
+
+// Find matches for a regular expression in a string and push their details to parts
+// Type is "a" for IDs, "b" for classes, attributes and pseudo-classes and "c" for elements and pseudo-elements
+var findMatch = function(regex, type, typeCount, selector) {
+	if (regex.test(selector)) {
+		var matches = selector.match(regex);
+		for (var i = 0; i < matches.length; i++) {
+			typeCount[type]++;
+			// Replace this simple selector with whitespace so it won't be counted in further simple selectors
+			selector = selector.replace(matches[i], '');
+		}
+	}
+
+	return selector;
+}
+
 var calculate = function(input) {
-	var selectors,
-		selector,
-		i,
-		len,
-		results = [];
-
 	// Separate input by commas
-	selectors = input.split(',');
+	var selectors = input.split(',');
+	var results = [];
 
-	for (i = 0, len = selectors.length; i < len; i += 1) {
-		selector = selectors[i];
-		if (selector.length > 0) {
-			results.push(calculateSingle(selector));
+	for (var i = 0; i < selectors.length; i++) {
+		if (selectors[i].length > 0) {
+			results.push(calculateSingle(selectors[i]));
 		}
 	}
 
@@ -27,82 +69,40 @@ var calculate = function(input) {
 
 // Calculate the specificity for a selector by dividing it into simple selectors and counting them
 var calculateSingle = function(selector) {
-	var findMatch,
-		typeCount = {
-			a: 0,
-			b: 0,
-			c: 0
-		},
-		// The following regular expressions assume that selectors matching the preceding regular expressions have been removed
-		attributeRegex = /(\[[^\]]+\])/g,
-		idRegex = /(#[^\s\+>~\.\[:]+)/g,
-		classRegex = /(\.[^\s\+>~\.\[:]+)/g,
-		pseudoElementRegex = /(::[^\s\+>~\.\[:]+|:first-line|:first-letter|:before|:after)/g,
-		pseudoClassRegex = /(:[^\s\+>~\.\[:]+)/g,
-		elementRegex = /([^\s\+>~\.\[:]+)/g;
-
-	// Find matches for a regular expression in a string and push their details to parts
-	// Type is "a" for IDs, "b" for classes, attributes and pseudo-classes and "c" for elements and pseudo-elements
-	findMatch = function(regex, type) {
-		var matches, i, len, match, index, length;
-		if (regex.test(selector)) {
-			matches = selector.match(regex);
-			for (i = 0, len = matches.length; i < len; i += 1) {
-				typeCount[type] += 1;
-				match = matches[i];
-				index = selector.indexOf(match);
-				length = match.length;
-				// Replace this simple selector with whitespace so it won't be counted in further simple selectors
-				selector = selector.replace(match, Array(length + 1).join(' '));
-			}
-		}
+	var	typeCount = {
+		a: 0,
+		b: 0,
+		c: 0
 	};
 
-	// Remove the negation psuedo-class (:not) but leave its argument because specificity is calculated on its argument
-	(function() {
-		var regex = /:not\(([^\)]*)\)/g;
-		if (regex.test(selector)) {
-			selector = selector.replace(regex, '     $1 ');
-		}
-	}());
+	selector = removeNot(selector);
 
-	// Remove anything after a left brace in case a user has pasted in a rule, not just a selector
-	(function() {
-		var regex = /{[^]*/gm,
-			matches, i, len, match;
-		if (regex.test(selector)) {
-			matches = selector.match(regex);
-			for (i = 0, len = matches.length; i < len; i += 1) {
-				match = matches[i];
-				selector = selector.replace(match, Array(match.length + 1).join(' '));
-			}
-		}
-	}());
+	selector = removeRules(selector);
 
 	// Add attribute selectors to parts collection (type b)
-	findMatch(attributeRegex, 'b');
+	selector = findMatch(attributeRegex, 'b', typeCount, selector);
 
 	// Add ID selectors to parts collection (type a)
-	findMatch(idRegex, 'a');
+	selector = findMatch(idRegex, 'a', typeCount, selector);
 
 	// Add class selectors to parts collection (type b)
-	findMatch(classRegex, 'b');
+	selector = findMatch(classRegex, 'b', typeCount, selector);
 
 	// Add pseudo-element selectors to parts collection (type c)
-	findMatch(pseudoElementRegex, 'c');
+	selector = findMatch(pseudoElementRegex, 'c', typeCount, selector);
 
 	// Add pseudo-class selectors to parts collection (type b)
-	findMatch(pseudoClassRegex, 'b');
+	selector = findMatch(pseudoClassRegex, 'b', typeCount, selector);
 
 	// Remove universal selector and separator characters
-	selector = selector.replace(/[\*\s\+>~]/g, ' ');
+	selector = selector.replace(separatorRegex, ' ');
 
 	// Remove any stray dots or hashes which aren't attached to words
 	// These may be present if the user is live-editing this selector
-	selector = selector.replace(/[#\.]/g, ' ');
+	selector = selector.replace(straysRegex, ' ');
 
 	// The only things left should be element selectors (type c)
-	findMatch(elementRegex, 'c');
+	findMatch(elementRegex, 'c', typeCount, selector);
 
 	return (typeCount.a * 100) + (typeCount.b * 10) + (typeCount.c * 1);
 };
